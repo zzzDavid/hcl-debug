555f7f81b2e1ffda2828f520785790689d4eaaa2
Niansong Zhang <nz264@cornell.edu>
2022-09-02 15:58:30 -0400

    Add linenumber

---
+++ linenumber/ln_example.py
@@ -0,0 +1,29 @@
+import os
+import sys
+ 
+ 
+def fileline(frame=0):
+    fr = sys._getframe(frame+1) # +1 to ignore this function call ...
+    return (os.path.basename(fr.f_code.co_filename), fr.f_lineno)
+ 
+class foo:
+    def __init__(self, v):
+        self._v = v
+        self.declaration = fileline()
+ 
+    @property
+    def v(self):
+        return self._v
+ 
+    @v.setter
+    def v(self, value):
+        self._v = value
+        self.lastassign = fileline(1)   # 1=file/line of the immediate caller of this function
+ 
+x = foo(4)
+ 
+x.v = 5
+print(f"{x.lastassign}")
+ 
+x.v = 6
+print(f"{x.lastassign}")

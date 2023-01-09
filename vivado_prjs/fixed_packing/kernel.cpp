b630b685ac6181d23905bbcf27ec9c43bf604760
Niansong Zhang <nz264@cornell.edu>
2023-01-09 18:10:27 -0500

    update

--- vivado_prjs/fixed_packing/kernel.cpp
+++ vivado_prjs/fixed_packing/kernel.cpp
@@ -4,13 +4,15 @@ void kernel(ap_fixed<5, 2> inputs[3], ap_fixed<5, 2> outputs[3])
 {
     // pack three elements
     ap_uint<15> packed;
+    union {ap_uint<5> ui; ap_int<5> si;} un;
     for (int i = 0; i < 3; i++) {
-        // printf("inputs[i] << 3: %d \n", (ap_uint<5>)(inputs[i] * 8));
-        packed(5*i+4, 5*i) = inputs[i] * 8;
+        un.si = (ap_int<5>)(inputs[i] * 8);
+        packed(5*i+4, 5*i) = un.ui;
     }
     // unpack three elements
     for (int i = 0; i < 3; i++) {
-        outputs[i] = ((ap_fixed<5, 2>)(packed(5*i+4, 5*i))) / 8;
+        un.ui = packed(5*i+4, 5*i);
+        outputs[i] = ((ap_fixed<5, 2>)(un.si)) / 8;
     }
 
     // // pack three elements
